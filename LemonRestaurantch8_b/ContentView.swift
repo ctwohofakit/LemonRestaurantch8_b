be0a0a6731878b95c1369fd9c8b9b8f56cda13bb
kit sitou <ctwohofa@gmail.com>
2026-03-19 20:22:22 -0700

    Initial Commit

---
+++ LemonRestaurantch8_b/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LemonRestaurantch8_b
+//
+//  Created by Kit Sitou on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LemonRestaurantch8_b/LemonRestaurantch8_bApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantch8_bApp.swift
+//  LemonRestaurantch8_b
+//
+//  Created by Kit Sitou on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantch8_bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
